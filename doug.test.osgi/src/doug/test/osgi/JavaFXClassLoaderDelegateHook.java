4fb02de553ec04c73677dd4c1a34d683f80af9b7
Doug Schaefer <cdtdoug@gmail.com>
2013-12-31 22:03:14 -0500

    Make this work for Luna.

--- doug.test.osgi/src/doug/test/osgi/JavaFXClassLoaderDelegateHook.java
+++ doug.test.osgi/src/doug/test/osgi/JavaFXClassLoaderDelegateHook.java
@@ -1,77 +1,24 @@
 package doug.test.osgi;
 
-import java.io.FileNotFoundException;
-import java.net.URL;
-import java.util.Enumeration;
-
-import org.eclipse.osgi.baseadaptor.HookConfigurator;
-import org.eclipse.osgi.baseadaptor.HookRegistry;
-import org.eclipse.osgi.framework.adaptor.BundleClassLoader;
-import org.eclipse.osgi.framework.adaptor.BundleData;
-import org.eclipse.osgi.framework.adaptor.ClassLoaderDelegateHook;
+import org.eclipse.osgi.internal.hookregistry.ClassLoaderHook;
+import org.eclipse.osgi.internal.hookregistry.HookConfigurator;
+import org.eclipse.osgi.internal.hookregistry.HookRegistry;
+import org.eclipse.osgi.internal.loader.ModuleClassLoader;
 
 @SuppressWarnings("restriction")
-public class JavaFXClassLoaderDelegateHook implements ClassLoaderDelegateHook, HookConfigurator {
+public class JavaFXClassLoaderDelegateHook extends ClassLoaderHook implements HookConfigurator {
 
 	@Override
 	public void addHooks(HookRegistry hookRegistry) {
-		hookRegistry.addClassLoaderDelegateHook(this);
+		hookRegistry.addClassLoaderHook(this);
 	}
 
 	@Override
-	public Class<?> preFindClass(String name, BundleClassLoader classLoader, BundleData data)
-			throws ClassNotFoundException {
+	public Class<?> postFindClass(String name, ModuleClassLoader classLoader) throws ClassNotFoundException {
 		if (name.startsWith("javafx."))
-			return java.lang.ClassLoader.getSystemClassLoader().loadClass(name);
-		return null;
-	}
-
-	@Override
-	public Class<?> postFindClass(String name, BundleClassLoader classLoader, BundleData data)
-			throws ClassNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public URL preFindResource(String name, BundleClassLoader classLoader, BundleData data)
-			throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public URL postFindResource(String name, BundleClassLoader classLoader, BundleData data)
-			throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Enumeration<URL> preFindResources(String name, BundleClassLoader classLoader, BundleData data)
-			throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Enumeration<URL> postFindResources(String name, BundleClassLoader classLoader, BundleData data)
-			throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String preFindLibrary(String name, BundleClassLoader classLoader, BundleData data)
-			throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String postFindLibrary(String name, BundleClassLoader classLoader, BundleData data) {
-		// TODO Auto-generated method stub
-		return null;
+			return ClassLoader.getSystemClassLoader().loadClass(name);
+		else
+			return null;
 	}
 
 }
